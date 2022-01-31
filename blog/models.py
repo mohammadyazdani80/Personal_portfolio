d475b4219760959d677f22ba8db8a42ab94ba721
Mohammad <mohammadycontact@gmail.com>
2022-01-31 22:57:30 +0330

    v 1.1

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
+from time import time
 from django.db import models
 from matplotlib.pyplot import title
 
 class Blog (models.Model):
     title = models.CharField(max_length = 100)
     discriptions = models.CharField(max_length = 300,default='' )
+    time = models.DateTimeField(auto_now=True)
     url = models.URLField(blank=True)
